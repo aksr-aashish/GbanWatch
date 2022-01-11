6214af95d3304289600967f6e62ddb99792a6693
Sourcery AI <>
2022-01-11 14:15:26 +0000

    'Refactored by Sourcery'

--- Sibyl_System/plugins/main_manager.py
+++ Sibyl_System/plugins/main_manager.py
@@ -87,10 +87,7 @@ async def scan(event):
         target = replied.sender.id
     executer = await event.get_sender()
     req_proof = req_user = False
-    if "f" in flags.keys() and executer.id in INSPECTORS:
-        approve = True
-    else:
-        approve = False
+    approve = "f" in flags.keys() and executer.id in INSPECTORS
     if replied.media:
         await replied.forward_to(GbanWatch_log)
     executor = f"[{executer.first_name}](tg://user?id={executer.id})"
@@ -150,33 +147,32 @@ async def approve(event):
     match = re.match(r"\$SCAN", replied.text)
     auto_match = re.search(r"\$AUTO(SCAN)?", replied.text)
     me = await System.get_me()
-    if auto_match:
-        if replied.sender.id == me.id:
-            id = re.search(
-                r"\*\*Scanned user:\*\* (\[\w+\]\(tg://user\?id=(\d+)\)|(\d+))",
-                replied.text,
-            ).group(2)
-            try:
-                message = re.search(
-                    "(\*\*)?Message:(\*\*)? (.*)", replied.text, re.DOTALL
-                ).group(3)
-            except:
-                message = None
-            try:
-                bot = (await System.get_entity(id)).bot
-            except:
-                bot = False
-            reason = re.search("\*\*Reason:\*\* (.*)", replied.text).group(1)
-            await System.gban(
-                enforcer=me.id,
-                target=id,
-                reason=reason,
-                msg_id=replied.id,
-                auto=True,
-                bot=bot,
-                message=message,
-            )
-            return "OwO"
+    if auto_match and replied.sender.id == me.id:
+        id = re.search(
+            r"\*\*Scanned user:\*\* (\[\w+\]\(tg://user\?id=(\d+)\)|(\d+))",
+            replied.text,
+        ).group(2)
+        try:
+            message = re.search(
+                "(\*\*)?Message:(\*\*)? (.*)", replied.text, re.DOTALL
+            ).group(3)
+        except:
+            message = None
+        try:
+            bot = (await System.get_entity(id)).bot
+        except:
+            bot = False
+        reason = re.search("\*\*Reason:\*\* (.*)", replied.text).group(1)
+        await System.gban(
+            enforcer=me.id,
+            target=id,
+            reason=reason,
+            msg_id=replied.id,
+            auto=True,
+            bot=bot,
+            message=message,
+        )
+        return "OwO"
     if match:
         reply = replied.sender.id
         sender = await event.get_sender()
@@ -197,13 +193,9 @@ async def approve(event):
                 reason = re.search(
                     r"(\*\*)?(Scan)? ?Reason:(\*\*)? (`([^`]*)`|.*)", replied.text
                 )
-                reason = reason.group(5) if reason.group(5) else reason.group(4)
-            if len(list) > 1:
-                id1 = list[0]
-                id2 = list[1]
-            else:
-                id1 = list[0]
-                id2 = re.findall(r"(\d+)", replied.text)[1]
+                reason = reason.group(5) or reason.group(4)
+            id1 = list[0]
+            id2 = list[1] if len(list) > 1 else re.findall(r"(\d+)", replied.text)[1]
             if id1 in ENFORCERS or SIBYL:
                 enforcer = id1
                 scam = id2
--- Sibyl_System/utils.py
+++ Sibyl_System/utils.py
@@ -7,8 +7,6 @@ FLAG_REGEX = re.compile(r"-\w+")
 
 def seprate_flags(message: str) -> Tuple[Dict[str, bool], str]:
     flags = FLAG_REGEX.findall(message)
-    flags_dict = dict()
-    for flag in flags:
-        flags_dict[flag[1:]] = True
+    flags_dict = {flag[1:]: True for flag in flags}
     message = FLAG_REGEX.sub("", message)
     return (flags_dict, message)
